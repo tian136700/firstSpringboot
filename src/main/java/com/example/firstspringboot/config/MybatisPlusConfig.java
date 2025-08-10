6d1fb69811488e1d9afd0dbd5f73086c780bd3b1
apple <12222@qq.com>
2025-08-10 23:49:18 +0700

    第一次提交

---
+++ src/main/java/com/example/firstspringboot/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.example.firstspringboot.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@MapperScan 就像是告诉 Spring Boot："去这个包里找所有的 Mapper 接口，并且自动帮我管理它们
+@MapperScan("com.example.firstspringboot.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
